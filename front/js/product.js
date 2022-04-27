3430e6901bd62b5270c5cceb34a7b1bdaa9ab7a8
SparqueeZ <baptiste.dijouxn8@gmail.com>
2022-04-27 16:04:03 +0400

    Ajout : Appel de la méthode get avec le fichier ajax.js, Affichage du produit dans la page, Ajout d'un evenement click, si la quantité n'as pas été remplie, message erreur

--- front/js/product.js
+++ front/js/product.js
@@ -1,19 +1,63 @@
 const showProductInfos = async () => {
 
-    const getURL = window.location.href;
+    const alertMessageNoId = "Vous n'avez pas choisi de produit, veuillez en choisir un à la page d'acceuil";
+    const alertMessageWrongId = "Le produit sélectionné n'existe pas, veuillez en choisir un autre à la page d'acceuil";
+    const alertMessageWrongNumber = "Vous devez sélectionner au moins 1 produit"
+    const homeLink = "file:/C:/Users/bapti/Documents/OpenClassRooms/P5/front/html/index.html";
 
-    let searchParams = new URLSearchParams(window.location.search);
+    const searchParams = new URLSearchParams(window.location.search);
+
     if (searchParams.has('id')) {
-        let id = searchParams.get('id');
-        console.log(id)
+        // ERREUR : Non fonctionnel
+        if (searchParams.has('id' && 'undefined')) {
+            window.alert(alertMessageWrongId);
+            document.location.href = homeLink;
+        }
+
+        else {
+            let idProduct = searchParams.get('id');
+            console.log("id du produit = " + idProduct);
+
+            const product = await get('http://localhost:3000/api/products' + '/' + idProduct);
+
+            //Intégrer les images
+            document.getElementsByClassName('item__img')[0].innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+            //Intégrer les noms
+            document.getElementById('title').innerHTML = `${product.name}`;
+            //Intégrer les prix
+            document.getElementById('price').innerHTML = `${product.price}`;
+            //Intégrer les descriptions
+            document.getElementById('description').innerHTML = `${product.description}`;
+
+            //Intégrer les couleurs (ERREUR : Une couleur est mise par défaut, hors par défaut ça doit être : --SVP ...)
+            let colorsHTML = '';
+            product.colors.forEach(color => {
+                colorsHTML += `<option value="${color}">${color}</option>`
+                document.getElementById('colors').innerHTML = colorsHTML;
+            });
+
+            //Ajouter un événement click sur le bouton. ERREUR : Toutes les infos ne sont pas dans un array 
+            document.getElementById('addToCart').addEventListener("click", () => {
+                let articleNumber = document.getElementById('quantity').value;
+                if (articleNumber >= 1 && articleNumber <= 100) {
+                    console.log(product);
+                    console.log(articleNumber);
+                    console.log(colorsHTML.value);
+
+                }
+                else {
+                    window.alert(alertMessageWrongNumber);
+                }
+            });
+        }
+
+
     }
     else {
-        window.alert("Vous n'avez pas choisi de produit, veuillez en choisir un à la page d'acceuil");
-        document.location.href = "file:///C:/Users/bapti/Documents/OpenClassRooms/P5/front/html/index.html";
+        window.alert(alertMessageNoId);
+        document.location.href = homeLink;
     }
 
-
 }
-
 
 showProductInfos();
--- front/js/script.js
+++ front/js/script.js
@@ -1,13 +1,7 @@
 const showListProduct = async () => {
     const productList = await get('http://localhost:3000/api/products');
-    // console.log(productList.map((product) => `<a href="./product.html?id=${product._id}">
-    //         <article>
-    //             <h3 class="productName" >${product.name} </h3>
-    //             <img src="${product.imageUrl}" alt="${product.altTxt}">
-    //             <p class="productDescription">${product.description}</p>  
-    //         </article>`));
 
-    const test = document.getElementById('items');
+    const getItems = document.getElementById('items');
     let productHTML = '';
     productList.forEach(product => {
         productHTML += `<a href="./product.html?id=${product._id}">
@@ -18,7 +12,7 @@ const showListProduct = async () => {
              </article>`
     })
 
-    test.innerHTML = productHTML;
+    getItems.innerHTML = productHTML;
 
 
     // document.getElementById('items').innerHTML = productList.map((product) => `<a href="./product.html?id=${product._id}">
