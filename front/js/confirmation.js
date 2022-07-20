a624de4989c1c2e47f100d096e9872b1a54c1fb4
SparqueeZ <baptiste.dijouxn8@gmail.com>
2022-07-20 14:14:02 +0400

    Retrait du console.log de trop

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -2,7 +2,6 @@
 const showOrderIdOnPage = () => {
     //Récupération de l'order ID dans l'URL
     const orderId = new URLSearchParams(window.location.search).get("orderId");
-    console.log(orderId);
     //Insertion dans l'élément ayant pour ID "orderId"
     document.getElementById('orderId').innerHTML = `<br><br>${orderId}`;
     if (orderId === null || orderId === '') {
